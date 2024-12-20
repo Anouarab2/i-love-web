ebf43d89a185c94c338df121cd355ded48d6313c
Anouarab2 <anouar.ait.brahim@hva.nl>
2024-12-20 10:52:26 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
+//selecteer de elementen die ik wil gebruiken
 const hamMenu = document.querySelector('.ham-menu');
 const hamItems = document.querySelector('.ham-items');
 
+//voeg een evenlistener toe die voor een functie zorgt bij het klikken
 hamMenu.addEventListener('click', openMenu)
 
+//benoem wat de functie moet doen
 function openMenu(){
     hamMenu.classList.toggle('active')
     hamItems.classList.toggle('active')
