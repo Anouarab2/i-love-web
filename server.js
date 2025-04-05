e551b19cc4614a3b324e5ac40fd224fcef3a013e
Anouarab2 <anouar.ait.brahim@hva.nl>
2025-04-05 22:28:46 +0200

    practicum

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ import express, { response } from 'express'
 
 import { Liquid } from 'liquidjs';
 
+import { readdir, readFile } from 'node:fs/promises'
+
+import { marked } from 'marked'
+
+const files = await readdir('content')
+
+console.log(files)
 
 const app = express()
 
@@ -12,16 +19,21 @@ app.engine('liquid', engine.express());
 
 app.set('views', './views')
 
-
-app.get('/journal', async function(req, res) {
-
-    res.render('journal.liquid')
-})
-
 app.get('/', async function(req, res) {
-
     res.render('index.liquid')
 })
+
+app.get('/journal', async function(req, res) {
+    res.render('journal.liquid', {files: files})
+})
+
+app.get('/journal/:slug', async function(req, res) {
+    console.log(req.params.slug)
+    const fileContents = await readFile('content/' + req.params.slug + '.md', { encoding: 'utf8' })
+    const opgemaakteContent = marked.parse(fileContents)
+    res.render('artikel.liquid',{fileContents: opgemaakteContent} )
+})
+
 
 app.use(express.urlencoded({extended: true}))
 
